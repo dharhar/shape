5d73ddc3b6a770b01861a847693b128d8eed2cc4
dave <ydrah32@gmail.com>
2018-01-16 13:32:50 -0600

    should automatically find aseg

--- enigma/medial.sh
+++ enigma/medial.sh
@@ -12,8 +12,8 @@ datadir=$1
 subj=$2
 outdir=$3
 runDirectory=$4
-#for testing:
-#roistring="10 26"
+asegfile=`find ${datadir}/${subj} -name aseg.mgz -print`
 
-perl /${runDirectory}/MedialDemonsShared/bin/Medial_Demons_shared.pl ${datadir}/${subj}/mri/aseg.mgz ${roistring} ${outdir} /${runDirectory}/MedialDemonsShared/ /usr/local/freesurfer/bin/
+perl /${runDirectory}/MedialDemonsShared/bin/Medial_Demons_shared.pl ${asegfile} ${roistring} ${outdir} /${runDirectory}/MedialDemonsShared/ /usr/local/freesurfer/bin/
 
+#perl /${runDirectory}/MedialDemonsShared/bin/Medial_Demons_shared.pl ${datadir}/${subj}/mri/aseg.mgz ${roistring} ${outdir} /${runDirectory}/MedialDemonsShared/ /usr/local/freesurfer/bin/
